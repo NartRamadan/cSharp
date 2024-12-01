670d1f92f58f354ad4861bb877c2c1f18cba3f42
NartRamadan <kataz91@gmail.com>
2024-12-01 15:19:37 +0200

    הגדרת תכונות

--- SHI3OR1/Program.cs
+++ SHI3OR1/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
 
-Console.WriteLine("Hello, World!");
+
+
+
+class car
+{
+    private string licensePlate;
+    private string model;
+    private int seat_num;
+    private int km;
+    private string color;
+    private bool isAvail;
+    private double dailyPrice;
+
+    void SetTaken()
+    {
+        this.isAvail = false;
+    }
+}
+
